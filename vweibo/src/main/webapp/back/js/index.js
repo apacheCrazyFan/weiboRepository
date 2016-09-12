4c0fdf4de3735be701277277ad9e68fe9f5e0882
zhoudan13 <18573113451@163.com>
2016-09-12 19:02:12 +0800

    theme.html页面  话题管理                                   --zd

--- vweibo/src/main/webapp/back/js/index.js
+++ vweibo/src/main/webapp/back/js/index.js
@@ -6,7 +6,8 @@ $(function(){
 		href:base+'backWelcome.html'
 	});
 	
-	$("#menu_tree").tree({
+	$(".easyui-tree").tree({//这里之前写错了，id要唯一，要不取值的时候，永远只会拿到第一个id的元素，这里是
+		//给tree增加点击事件，
 		onClick:function(node){
 			var text=node.text;  //获取选中节点的文本值
 			var tabs=$("#center_area");
@@ -30,17 +31,17 @@ $(function(){
 						closable:true
 					});
 				}
-			}/*else if(text=="管理员管理"){
-				if(tabs.tabs('exists','管理员管理')){//如果存在，则选中
-					tabs.tabs('select','管理员管理');
+			}else if(text=="话题标签管理"){
+				if(tabs.tabs('exists','话题标签管理')){//如果存在，则选中
+					tabs.tabs('select','话题标签管理');
 				}else{
 					tabs.tabs('add',{
-						title:"管理员管理",
-						href:"manager.html",
+						title:"话题标签管理",
+						href:base+"theme.html",
 						closable:true
 					});
 				}
-			}else if(text=="新闻类型管理"){
+			}/*else if(text=="新闻类型管理"){
 				if(tabs.tabs('exists','新闻类型管理')){//如果存在，则选中
 					tabs.tabs('select','新闻类型管理');
 				}else{
